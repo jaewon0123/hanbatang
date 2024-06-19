9fe2786a523d7ddf068eb9f3effb3d9397954427
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-19 15:53:29 +0900

    start project

---
+++ hanbatang/src/test/java/com/hanbatang/HanbatangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbatang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbatangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
