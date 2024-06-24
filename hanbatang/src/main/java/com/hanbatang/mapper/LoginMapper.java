178a597b3d36894f7f392221c83ef0cbb1c71227
sangjunchung <wjtkwn19@gmail.com>
2024-06-25 08:52:05 +0900

    login

--- hanbatang/src/main/java/com/hanbatang/mapper/LoginMapper.java
+++ hanbatang/src/main/java/com/hanbatang/mapper/LoginMapper.java
@@ -1,8 +1,13 @@
 package com.hanbatang.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.hanbatang.dto.Members;
 
 @Mapper
 public interface LoginMapper {
-	
+	Members getLogin(@Param("member_id") String member_id, @Param("member_pw") String member_pw);
+	Members getFindId(@Param("member_name") String member_name, @Param("member_email") String member_email);
+	Members getFindPw(@Param("member_id") String member_id, @Param("member_name") String member_name);
 }
---
+++ hanbatang/src/main/java/com/hanbatang/service/LoginService.java
@@ -0,0 +1,24 @@
+package com.hanbatang.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hanbatang.dto.Members;
+import com.hanbatang.mapper.LoginMapper;
+
+@Service
+public class LoginService {
+	@Autowired
+	private LoginMapper loginMapper;
+	
+	public Members getLogin(String member_id, String member_pw) {
+		return loginMapper.getLogin(member_id, member_pw);
+	}
+	
+	public Members getFindId(String member_name, String member_email) {
+		return loginMapper.getFindId(member_name, member_email);
+	}
+	public Members getFindPw(String member_id, String member_name) {
+		return loginMapper.getFindPw(member_id, member_name);
+	}
+}
