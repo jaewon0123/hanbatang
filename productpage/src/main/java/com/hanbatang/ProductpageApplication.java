3ec9a5031407fbd86c4e2464f7b8f1364151f1e9
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-24 18:03:00 +0900

    js 작동 css 적용

--- productpage/src/main/java/com/hanbatang/ProductpageApplication.java
+++ productpage/src/main/java/com/hanbatang/ProductpageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HanbatangApplication {
+public class ProductpageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HanbatangApplication.class, args);
+		SpringApplication.run(ProductpageApplication.class, args);
 	}
 
 }
--- productpage/src/test/java/com/hanbatang/ProductpageApplicationTests.java
+++ productpage/src/test/java/com/hanbatang/ProductpageApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HanbatangApplicationTests {
+class ProductpageApplicationTests {
 
 	@Test
 	void contextLoads() {
